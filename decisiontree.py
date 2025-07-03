74ecdc23fbe006d7a152c21e0460538b210429be
amn-codes <aman@botni.vision>
2025-07-03 17:16:50 -0400

    minor: updating to switch machines

--- decisiontree.py
+++ decisiontree.py
@@ -27,7 +27,7 @@ class DecisionTree:
     def __init__(self, criterion='gini', splitter="best", max_depth=None, min_samples_split=2, min_samples_leaf=1, max_features=None, random_state=0, min_impurity_descrease=0, class_weight=None):
         self.tree = None
         self.criterion = criterion
-        self.
+        pass
 
     def fit(self, X, y):
         pass
